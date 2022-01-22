14890cde487da99fc27f4bb2fe07130d4bc5379f
kesh-khanna <65061082+kesh-khanna@users.noreply.github.com>
2022-01-22 10:50:54 -0500

    tweaked to add accurate phone number

--- send_sms.py
+++ send_sms.py
@@ -1,23 +1,16 @@
-
-# Download the helper library from https://www.twilio.com/docs/python/install
 import os
 from twilio.rest import Client
 
-import os
-from twilio.rest import Client
-
-
-# Find your Account SID and Auth Token at twilio.com/console
-# and set the environment variables. See http://twil.io/secure
+# See Environment Variables
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
 client = Client(account_sid, auth_token)
 
 message = client.messages \
                 .create(
-                     body="Join Earth's mightiest heroes. Like Kevin Bacon.",
-                     from_='+15017122661',
-                     to='+15558675310'
+                     body="Test",
+                     from_='+16067312179',
+                     to='+12023049104'
                  )
 
 print(message.sid)
