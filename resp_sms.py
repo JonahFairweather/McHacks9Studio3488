68ff7372d9333d6e7750ef5e9e99c2d2bd5c1814
kesh-khanna <65061082+kesh-khanna@users.noreply.github.com>
2022-01-22 22:59:35 -0500

    Program to scrape WritingPrompts subreddit for prompts and comment content

--- resp_sms.py
+++ resp_sms.py
@@ -1,5 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 from twilio import twiml
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
 
 # allows us to receive the message then respond in a dynamic way
 
@@ -8,12 +10,11 @@ app = Flask(__name__)
 
 @app.route('/sms', methods=['POST', "GET"])
 def sms():
-    number = request.form["From"]
-    message_body = request.form["Body"]
+    message_body = request.form.get("Body")
+    resp = MessagingResponse()
 
-    resp = twiml.Response()
-    resp.message("Hello {}, you said {}".format(number, message_body))
-    return str(resp)
+    resp.message("Hello back to you!")
+    return Response(str(resp), mimetype="application/xml"), 200
 
 
 if __name__ == '__main__':
--- send_sms.py
+++ send_sms.py
@@ -1,17 +1,44 @@
-import os
 from twilio.rest import Client
+import praw
+import os
+
+# using the praw library and a script created using the reddit api we are able to use our local host to webscrape reddit
+# this creates a Reddit instance
+reddit = praw.Reddit(
+    client_id="3YhYN_v6icb9G4RZmqHnyw",
+    client_secret="hxNP6hdsr9O37akYy_da7ArV79tPKQ",
+    user_agent="SubScraper"
+)
+
+# for adding to the final message
+prompt = ""
+
+def get_prompt():
+    global prompt
+    # finds the days top rated post in r/WritingPrompts and returns it as a PRAW Subreddit instance
+    top = reddit.subreddit('WritingPrompts').top("day", limit=1)
+    for post in top:
+        # trims off the standard header used i r/WritingPrompts
+        prompt = post.title[5::]
+    return prompt
+
+
+print(get_prompt())
 
 # See Environment Variables
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
+
+# creates and instance of Client from the twilio library
 client = Client(account_sid, auth_token)
 
 # sends message to our client
 message = client.messages \
     .create(
-    body="Test",
+    body=prompt,
     from_='+16067312179',
     to='+12023049104'
 )
 
+# to ensure we have sent the message correctly we print out the ID which can be verified on the Twilio platform
 print(message.sid)
