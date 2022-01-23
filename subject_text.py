c945c947cabfece3afd19e919ee4233620b5abfa
skycrol <yhourri2@gmail.com>
2022-01-22 23:12:05 -0500

    Gets the top3 most used nouns, verbs and proper nouns

--- subject_text.py
+++ subject_text.py
@@ -5,7 +5,8 @@ from nltk.corpus import stopwords
 from collections import Counter
 
 NOUNS = ['NN', 'NNS', 'NNP', 'NNPS']
-ELIMINATE_VERBS = ['have', 'be', 'go', 'look', 'see', 'hear', 'smell', 'touch', 'gone', 'like', 'love', 'begin', 'think']
+ELIMINATE_VERBS = ['have', 'be', 'go', 'look', 'see', 'hear', 'smell', 'touch', 'gone', 'like', 'love', 'begin',
+                   'think', 'say', 'make', 'mean']
 
 def pre_process(paragraph):
     paragraph = re.sub('[^A-Za-z .-]+', ' ', paragraph)
@@ -22,39 +23,10 @@ def word_freq(words):
     return Counter(words).most_common(1)
 
 
-def get_noun(paragraph):
-
-    dist = word_freq(paragraph)
-
-    most_freq = [w for w,c in dist.most_common(10)
-                 if nltk.pos_tag([w])[0][1] in NOUNS]
-
-    filter_sent = nltk.sent_tokenize(paragraph)
-
-
-    sentences = [nltk.word_tokenize(s) for s in filter_sent]
-    sentences = [nltk.pos_tag(s) for s in sentences]
-    return sentences
-
-
 def split_sentences(paragraph):
     sentences = nltk.sent_tokenize(paragraph)
     sentences = [nltk.word_tokenize(w) for w in sentences]
     return sentences
-
-
-def get_entities(paragraph):
-    categories = []
-    sentences = split_sentences(paragraph)
-    sentences = [nltk.pos_tag(s) for s in sentences]
-
-    for tagged_s in sentences:
-        for chunks in nltk.ne_chunk(tagged_s):
-            print(type(chunks))
-            if type(chunks) == nltk.tree.Tree:
-              categories.append(' '.join([c[0] for c in chunks]).lower())
-
-    return categories
 
 
 def get_nouns(paragraph):
@@ -62,62 +34,78 @@ def get_nouns(paragraph):
     nlp = spacy.load("en_core_web_sm")
     doc = nlp(paragraph)
 
-    sub_toks = [tok.lemma_ for tok in doc
-                if ((tok.dep_ == "nsubj" or tok.dep_ == "dobj" or tok.dep_ == "ROOT" or tok.dep_ == "pobj")
+    sub_toks = [tok.lemma_.lower() for tok in doc
+                if ((tok.dep_ == "nsubj" or tok.dep_ == "dobj" or tok.dep_ == "ROOT" or tok.dep_ == "pobj"
+                     or tok.dep_ == "attr" or tok.dep_ == "xcomp")
                     and (tok.pos_ == 'NOUN'))]
 
     return sub_toks
 
 
-def get_actions(paragraph):
+def get_verbs(paragraph):
     nlp = spacy.load("en_core_web_sm")
     doc = nlp(paragraph)
 
-    sub_toks = [tok.lemma_ for tok in doc
-                if (tok.dep_ == "attr" or tok.dep_ == "xcomp" or tok.dep_ == "ROOT" or tok.pos_ == 'VERB')]
+    sub_toks = [tok for tok in doc
+                if (tok.pos_ == 'VERB')]
+
+    # removing verbs with no real meaning about the text
+    for index,verbs in enumerate(sub_toks):
+        if verbs.pos_ == 'VERB':
+            indirect_object = False
+            direct_object = False
+        for obj in verbs.children:
+            if obj.dep_ == "iobj" or obj.dep_ == "pobj":
+                indirect_object = True
+            if obj.dep_ == "dobj" or obj.dep_ == "dative":
+                direct_object = True
+        # We do not want ditransitive verbs
+        if indirect_object and direct_object:
+            sub_toks.remove(verbs)
+        else:
+            sub_toks[index] = verbs.lemma_
+
+    for verb in sub_toks:
+        if verb in ELIMINATE_VERBS:
+            sub_toks.remove(verb)
     return sub_toks
 
 
 def get_entities(paragraph):
     nlp = spacy.load("en_core_web_sm")
     doc = nlp(paragraph)
-
-    ents = [e.text for e in doc.ents]
+    ents = [e.text.lower() for e in doc.ents]
 
     return ents
 
 
 def get_tags(paragraph):
-
+    tags = []
+    tags.append(get_verbs(paragraph))
     paragraph = pre_process(paragraph)
-    tags = get_nouns(paragraph)
-    tags.append(get_actions(paragraph))
+    tags.append(get_nouns(paragraph))
     tags.append(get_entities(paragraph))
     return tags
 
+def get_popular(paragraph):
+    top3 = []
+    tags = get_tags(paragraph)
+    for types in tags:
+        top3.append(Counter(types).most_common(3))
+    return top3
+
+
 
 if __name__ == '__main__':
-    nlp = spacy.load("en_core_web_sm")
-    doc = nlp("Harry Potter is straight shit")
+    text = 'The majority of galactic species evolved as hive mind,' \
+           ' the professor\'s voice rung throughout the metallic lecture hall, though only a few hundred of the thousands of students were physically present, the rest would be catching the beam. As a pie chart appeared on the vision boards the professor continued, "another sizeable portion evolved from pack dynamics apex predators. It is estimated that as few as half a percent of galactic civilizations developed interstellar technology from a tribal civilization." The professor raised a finger, and the smallest slice of the pie possible became highlighted, "even among that particular subset, only one emerged from a tribal civilization where the dominant tribe wasn\'t fully determined before the initial attempts at spacefaring were made.'\
+    'The vision boards changed, but the professor lowered his hand and they went momentarily dark, "before we continue, consider that point. For every civilization other than humanity, there is a singular, inescapable, gap - their warfighting capability, tactics, and motives, go utterly dormant and unchanged from at least the advent of spacefaring to their arrival on the interstellar stage, for most far longer than that. The hive minds can\'t imagine dealing with an equal being, the apex predators have never been hunted, even the very few tribal species haven\'t dealt with others on equal footing since times barely preserved as legend. Is it any wonder their solutions to conflict have been rudimentary, and swift? None had needed anything resembling either war or diplomacy in most of their evolutionary history.'\
+           'The boards lit up again, images famous in the human diaspora. The first battle of the twin stars, when the Canim fleet had come to eradicate humanities expansion into their sector. A ring of gleaming warships, whose FTL technology was primitive in comparison, but loaded with weapons the dogs had never imagined. Weapons honed from centuries of intersystem war, "There were three hundred and forty two nations on earth when the Admiral Parry entered FTL bound for Alpha Centauri. There are three hundred and thirty six today. Humanity never stopped learning how to fight. With the pieces we pulled out of the wreckage of the first Canim battle fleet our FTL reached parity fairly quickly."'\
+           'Another image, ships of similar design, but sleeker, newer, lighting fusion candles as they pulled away from the shining blue marble of earth in the distance, "the Admiral Perry may have been named similar to its predecessor, but it served a different purpose. An attempt was made to close the galaxy to us. Those ships went forth to kick it open."'\
+           'final image, the standard of Earth, a blue marble on a field of black originally marking a single system growing to take a sizeable chunk of the local arm of the milky may. A war to end all wars, a war of dominance. Humans had learned a few things about diplomacy in its history, yes - but it had also learned how to deal with predators below it on the food chain. Humans were an odd sort of apex predator, but they were not any less of one.'
 
-    text = "What do you mean? We outmatch you in firepower 10,000 to 1!" \
-           "mhm, valid point, but you only need one ten-thousandth of your power to destroy yourself anyways..."\
-           "What are you talking about?"\
-           "See, earth, the only reason we survived so long was because of this little concept we knew as 'Mutually assured destruction.' Essentially meaning, if one fires, so too does the other."\
-           "You would never be able to defend!"\
-           "That's not the point, the point is if we both have the ability to kill each other, then it doesn't matter if you can't defend so long as you can react, and retaliate before their attack hits you. And believe me, we can react in time." \
-            "The Ilerian paused. Thinking about what the pathetic human had said. he knew humans had nuclear armaments, and that they could destroy his species, but he had always thought that it didn't matter so long as they were more powerful."\
-           "Our weapons should chill you to the bone, why do you act so confident in the face of death?" \
-           "Because we've stared death in the face many times before. Humans are deadly, we've commit genocide on our own people multiple times. We've faced extinction, and planetary obliteration before, and never once did we back down. All this is is just another doomsday scenario that we will stare down until it either hits us, or back off."\
-           "The Ilerian was taken aback by these words. He had heard that humans were vicious, but never expected them to be so cunning in the face of war."\
-           "I... you can't possibly be serious. No species would drive themselves to the brink of extinction!"\
-           "Kind of ironic, seeing as you're doing it right now, testing us. Who says we wont make the first move?"\
-           "y-you wouldn't! We outgun you tenth-"\
-           "so long as we can destroy your entire civilization, firepower hardly matters. Essentially, that makes us equal. So as your equal peer, i suggest you disarm those planet breakers, and we discuss peace."\
-           "The Ilerian had ever heard someone speak of themselves as an equal to the Ilerian empire. They had outposts all over the quadrant, and this pathetic morsel thinks they can oppose him? But what if he wasn't bluffing? What if they did retaliate. They have the means and know-how to deploy untraceable warheads to every base they had. If the ilerians attacked first, would the humans be able to react in time?"\
-           "uh... well then human... i- uhh, I guess we are in stalemate. I declare we never interact aga-"\
-           "Oh no no, that's not how this works anymore. You just lost your chance to drop it and say that we never met, cause now I'm the one calling the shots. So here's my proposal. begin disarmament, and once you reach weapon equivalent to us, we too shall begin disarmament procedures. You will drop all of your trade barriers, and open your colonies to cultural exchange. If you don't I can assure you that neither of us will live to see the next galactic annum. Do we have a deal?"
-    print(get_tags(text))
+    print(get_popular(text))
+
 
 
 
